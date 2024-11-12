ece618399f1bd6eef99cdc0310f410e347e5b389
abhinav09 <shawabhinav93@gmail.com>
2024-11-12 23:30:20 +0530

    made some minor changes to the frontend for rendering the data

--- frontend/src/pages/MarketPlace/ProductListSection.jsx
+++ frontend/src/pages/MarketPlace/ProductListSection.jsx
@@ -1,14 +1,11 @@
-import { ProductList } from "../../api/ProductList";
 import ProductListCard from "./ProductListCard";
 
-const CONTENTS = ProductList;
-
-export const ProductListSection = () => {
+export const ProductListSection = ({ data }) => {
   return (
     <section className="py-10 px-8 flex flex-col min-h-screen">
       <div className="mx-auto container">
         <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 md:grid-cols-2">
-          {CONTENTS.map(({ img, name, price }, index) => (
+          {data.map(({ img, name, price }, index) => (
             <ProductListCard key={index} img={img} name={name} price={price} />
           ))}
         </div>
--- frontend/src/pages/MarketPlace/index.jsx
+++ frontend/src/pages/MarketPlace/index.jsx
@@ -1,11 +1,15 @@
 import { useFetch } from "../../hooks/useFetch";
 import { Spinner } from "@material-tailwind/react";
-import {ProductListSection} from "./ProductListSection"; 
+import { ProductListSection } from "./ProductListSection";
+import { ProductList } from "../../api/ProductList";
+
+const CONTENTS = ProductList;
 
 export const MarketPlace = () => {
   const { data, loading, error } = useFetch(
     "http://127.0.0.1:8000/api/products/"
   );
+  console.log("data: ", data.length);
 
   if (loading) {
     return (
@@ -18,5 +22,9 @@ export const MarketPlace = () => {
   if (error) {
     return <div>Error: {error.message}</div>;
   }
-  return <ProductListSection/>;
+  if (data !== null) {
+    var productsToDisplay = data.length ? data : CONTENTS;
+  }
+
+  return <ProductListSection data={productsToDisplay} />;
 };
